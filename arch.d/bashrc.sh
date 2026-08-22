6aeed2317c40e3c15e7c695b8886f496c3628bdf
Simone Gentili <gensimone.git@gmail.com>
2026-08-22 10:09:42 +0200

    Add bashrc.sh

---
+++ arch.d/bashrc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cat <<EOF | sudo tee /root/.bashrc
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+set -o vi
+
+PS1='\[\e[31m\][root]\[\e[0m\][\[\033[01;34m\]\w\[\033[00m\]]$ '
+EOF
